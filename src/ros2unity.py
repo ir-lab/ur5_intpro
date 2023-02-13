d5229b91850f18f4beac5448d7a9d82f8d505626
Shubham Sonawani <sdsonawa@asu.edu>
2023-02-13 13:04:16 -0700

    1. node for sending motion capture data and  from optitrack to unity 2. sending ur5 joints from ros to unity

--- src/ros2unity.py
+++ src/ros2unity.py
@@ -21,12 +21,14 @@ class ROS2Unity:
         self.pov_pub = rospy.Publisher("/pov_pose",CamPose,queue_size=1)
         self.ur5_joints = [0,0,0,0,0,0]
         self.pov_pose   = [0,0,0,0,0,0,1]
+        self.origin_pose   = [0,0,0,0,0,0,1]
         self.obj_pose   = [0,0,0,0,0,0,1]
         print("sleeping for 1 second....")
         rospy.sleep(1)
 
         rospy.Subscriber("/ur5/joints", ur5Joints, self.joints_callback)
         rospy.Subscriber("/vrpn_client_node/POV/pose", PoseStamped, self.pov_callback)
+        # rospy.Subscriber("/vrpn_client_node/Origin/pose", PoseStamped, self.orgin_callback)
         
 
     def joints_callback(self,msg):
@@ -43,6 +45,15 @@ class ROS2Unity:
         self.pov_pose[6]= msg.pose.orientation.z
 
 
+    def orgin_callback(self,msg):
+        self.origin_pose[0]= msg.pose.position.x
+        self.origin_pose[1]= msg.pose.position.y
+        self.origin_pose[2]= msg.pose.position.z
+        self.origin_pose[3]= msg.pose.orientation.w
+        self.origin_pose[4]= msg.pose.orientation.x
+        self.origin_pose[5]= msg.pose.orientation.y
+        self.origin_pose[6]= msg.pose.orientation.z
+        
     def ros2unity_ur5(self):
         rel_tf = compose(self.pov_pose[0:3], quat2mat(self.pov_pose[3:]), [1,1,1])
         pov_msg = CamPose()
@@ -55,6 +66,19 @@ class ROS2Unity:
         pov_msg.y_ = _q[2]
         pov_msg.z_ = _q[3]
         pov_msg.w_ = -_q[0]
+        
+        # tf_mat_org = compose([self.origin_pose[0:3]],quat2mat(self.origin_pose[-1,3,4,5]),[1,1,1])
+        # tf_mat_pov = compose([self.pov_pose[0:3]],quat2mat(self.pov_pose[-1,3,4,5]),[1,1,1])
+        
+        # rel_tf = np.matmul(tf_mat_org,np.linalg.inv(tf_mat_pov))
+        # pov_msg.x  = -rel_tf[0,-1]
+        # pov_msg.y  =  rel_tf[1,-1] 
+        # pov_msg.z  =  rel_tf[2,-1]
+        # _q = mat2quat(rel_tf[0:3,0:3])
+        # pov_msg.x_ = -_q[1]
+        # pov_msg.y_ = _q[2]
+        # pov_msg.z_ = _q[3]
+        # pov_msg.w_ = -_q[0]
         self.pov_pub.publish(pov_msg)
         print(f"pov pose: {pov_msg}")
         
